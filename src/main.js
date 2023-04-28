804e8bc84da69ac0349461e0a7249a9ed6e66f3b
afect13 <ns130192@gmail.com>
2023-04-28 16:05:17 +0500

    fix bug

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { Telegraf, session } from "telegraf";
 import { message } from "telegraf/filters";
-import config from "config";
 import { proccessVoiceMessage, proccessTextMessage } from "./logic.js";
 import config from "config";
 export const INITIAL_SESSION = {
@@ -8,27 +7,33 @@ export const INITIAL_SESSION = {
 };
 
 const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
-
+const allowedUsers = config.get("ALLOWED_USERS");
 bot.use(session());
 
-bot.command("new", async (ctx) => {
-  ctx.session = INITIAL_SESSION;
-  await ctx.reply("Жду вашего голосового сообщения");
-});
+bot.start((ctx) => {
+  const userId = ctx.from.id;
+  if (allowedUsers.includes(userId)) {
+    ctx.reply("Добро пожаловать!");
+    bot.command("new", async (ctx) => {
+      ctx.session = INITIAL_SESSION;
+      await ctx.reply("Жду вашего  сообщения");
+    });
+    bot.command("start", async (ctx) => {
+      ctx.session = INITIAL_SESSION;
+      await ctx.reply("Жду вашего  сообщения");
+    });
+    bot.on(message("voice"), async (ctx) => {
+      ctx.session ??= INITIAL_SESSION;
+      await proccessVoiceMessage(ctx);
+    });
 
-bot.command("start", async (ctx) => {
-  ctx.session = INITIAL_SESSION;
-  await ctx.reply("Жду вашего голосового сообщения");
-});
-
-bot.on(message("voice"), async (ctx) => {
-  ctx.session ??= INITIAL_SESSION;
-  await proccessVoiceMessage(ctx);
-});
-
-bot.on(message("text"), async (ctx) => {
-  ctx.session ??= INITIAL_SESSION;
-  await proccessTextMessage(ctx);
+    bot.on(message("text"), async (ctx) => {
+      ctx.session ??= INITIAL_SESSION;
+      await proccessTextMessage(ctx);
+    });
+  } else {
+    ctx.reply("Извините, у вас нет доступа к этому боту.");
+  }
 });
 
 bot.launch();
